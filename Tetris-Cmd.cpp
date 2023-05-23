18e1adfd1591106eaf3a1f48b3eb509cbd5c41a8
Justin <justinsnes@gmail.com>
2023-05-23 12:02:55 -0400

    dont do using std from widely accepted stack overflow advice. also first vs code checkin

--- Tetris-Cmd.cpp
+++ Tetris-Cmd.cpp
@@ -1,22 +1,23 @@
 // Tetris-Cmd.cpp : This file contains the 'main' function. Program execution begins and ends there.
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <algorithm>
+//#include <curses.h>
 
 const int ROWS = 4;
-const int COLS = 4;
+const int COLUMNS = 4;
 
 // grids, rows, columns
-int tetrominoLibrary[7][ROWS][COLS];
+int tetrominoLibrary[7][ROWS][COLUMNS];
 int fieldWidth = 10;
 int fieldHeight = 20;
 
-vector<vector<int>> createPiece(int arr[ROWS][COLS])
+std::vector<std::vector<int>> createPiece(int arr[ROWS][COLUMNS])
 {
-    vector<vector<int>> createdPiece(ROWS, vector<int>(COLS));
+    std::vector<std::vector<int>> createdPiece(ROWS, std::vector<int>(COLUMNS));
 
     for (int i = 0; i < ROWS; ++i) {
-        for (int j = 0; j < COLS; ++j) {
+        for (int j = 0; j < COLUMNS; ++j) {
             createdPiece[i][j] = arr[i][j];
         }
     }
@@ -24,16 +25,16 @@ vector<vector<int>> createPiece(int arr[ROWS][COLS])
     return createdPiece;
 }
 
-vector<vector<int>> rotatePiece(vector<vector<int>> piece)
+std::vector<std::vector<int>> rotatePiece(std::vector<std::vector<int>> piece)
 {
-    // for a 90 degree rotation, transpose the 4x4 grid then reverse each row
-    vector<vector<int>> rotatedPiece(ROWS, vector<int>(COLS));
+    // for a 90 degree clockwise rotation, transpose the 4x4 grid then reverse each row
+    std::vector<std::vector<int>> rotatedPiece(ROWS, std::vector<int>(COLUMNS));
 
     for (int i = 0; i < ROWS; ++i) {
-        for (int j = 0; j < COLS; ++j) {
+        for (int j = 0; j < COLUMNS; ++j) {
             rotatedPiece[i][j] = piece[j][i];
         }
-        reverse(rotatedPiece[i].begin(), rotatedPiece[i].end());
+        std::reverse(rotatedPiece[i].begin(), rotatedPiece[i].end());
     }
 
     // the below ends up reversing columns rather than rows for our 2d vector setup
@@ -42,12 +43,12 @@ vector<vector<int>> rotatePiece(vector<vector<int>> piece)
     return rotatedPiece;
 }
 
-string generateAsciiBlock(vector<vector<int>> vec) {
-    string asciiBlock = "";
+std::string generateAsciiBlock(std::vector<std::vector<int>> vec) {
+    std::string asciiBlock = "";
 
     int rowCounter = 0;
-    vector<vector<int>>::const_iterator row;
-    vector<int>::const_iterator col;
+    std::vector<std::vector<int>>::const_iterator row;
+    std::vector<int>::const_iterator col;
     for (row = vec.begin(); row != vec.end(); ++row) {
         for (col = row->begin(); col != row->end(); ++col) {
             if (*col == 1)
@@ -67,6 +68,8 @@ int main()
 {
     std::cout << "12345 World!\n";
 
+    //initscr();
+
 #pragma region
     tetrominoLibrary[0][0][2] = 1; // --X-
     tetrominoLibrary[0][1][2] = 1; // --X-
@@ -79,7 +82,7 @@ int main()
     tetrominoLibrary[1][2][1] = 1; // ----
 #pragma endregion The seven shapes of Tetris
 
-    vector<vector<int>> activePiece = createPiece(tetrominoLibrary[1]);
+    std::vector<std::vector<int>> activePiece = createPiece(tetrominoLibrary[1]);
     std::cout << generateAsciiBlock(activePiece) << " End\n";
 
     activePiece = rotatePiece(activePiece);
